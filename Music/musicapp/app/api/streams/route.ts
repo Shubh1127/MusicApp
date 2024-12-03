99feb1996d9e6391ee5a0fd08187867934263e22
shubham <shubhamsinghmor2312@gmail.com>
2024-12-03 20:19:44 +0530

    added routes for upvote downvote

--- Music/musicapp/app/api/streams/route.ts
+++ Music/musicapp/app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 import {z} from 'zod';
+const YT_REGEX=new RegExp("https:\/\/www\.youtube\.com\/watch\?v=([^&]+)")
 
 const createStreamSchema=z.object({
         creatorId:z.string(),
@@ -9,11 +10,24 @@ const createStreamSchema=z.object({
 export async function POST(req:NextResponse){
     try{
         const data =createStreamSchema.parse(await req.json());
-        const isYt=data.url.includes("youtube");
-        prismaClient.stream.create({
-            userId:data.creatorId,
-            url:data.url,
-        })
+        const isYt=YT_REGEX.test(data.url);
+        if(!isYt){
+            return NextResponse.json({
+                message:"Wrong URL format"
+            },{
+
+                status:421
+            })
+        }
+        const extractedId=data.url.split("?v=")[1];
+       await  prismaClient.stream.create({
+            data:{
+                userId:data.creatorId,
+                url:data.url,
+                extractedId,
+                type:"Youtube"
+            }
+        });
     }catch (e){
         return NextResponse.json({
             message:"error while adding a stream"
@@ -21,4 +35,16 @@ export async function POST(req:NextResponse){
             status:411
         })
     }
+}
+export async function GET(req:NextResponse){
+    const creatorId=req.nextUrl.searchParams.get("creatorId");
+    const streams=await prismaClient.stream.findMany({
+        where:{
+                usrId:creatorId ?? ""
+        }
+    })
+    return NextResponse.json({
+        streams
+    })
+
 }
