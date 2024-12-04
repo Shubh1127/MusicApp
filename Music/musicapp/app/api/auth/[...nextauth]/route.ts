4224f37e6c6d2a1fee0c30693cbb0a340cb52f8f
shubham <shubhamsinghmor2312@gmail.com>
2024-12-04 20:43:32 +0530

    added signin params

--- Music/musicapp/app/api/auth/[...nextauth]/route.ts
+++ Music/musicapp/app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,13 @@ const handler = NextAuth({
           clientId: process.env.GOOGLE_CLIENT_ID ?? "",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-      ]
+      ],
+      callbacks:{
+        signIn(params){
+          console.log(params)
+          return true;
+        }
+      }
 })
 
 export { handler as GET, handler as POST }
